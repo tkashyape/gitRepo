626fc1b4b26d8984bb6b933473285f5e46891a2d
Tushar Kashyape <tkashyape@gmail.com>
2017-03-30 04:52:39 +0530

    html tags removed
    nothing..just fine

--- faltu1.php
+++ faltu1.php
@@ -8,12 +8,7 @@
 	$result=mysqli_query($conn,$query);
 	$row=mysqli_fetch_all($result,MYSQLI_ASSOC);
 	echo "<table style='width:50%' border='1px solid black' border-collapse='collapse'>
-  <tr>
-  	<th>Sr.No</th>
-    <th>Name</th>
-    <th>City</th> 
-    <th>Link</th>
-  </tr>";
+  
 	for($i=0;$i<mysqli_num_rows($result);$i++)
 		echo "<tr><td>".($i+1)."</td><td>".$row[$i]["patient_name"]."</td><td>".$row[$i]["patient_city"]."</td><td><a href=11.php?q=".$row[$i]["patient_id"].">View</a>"."</td></tr>";
 	echo "</table></html>";
